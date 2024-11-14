ac7819b22740d6eb17528c1ec067879ca690a616
Hugo Marta <150078628+SirAlabar@users.noreply.github.com>
2024-11-14 13:46:24 +0000

    Update main2.c

--- main2.c
+++ main2.c
@@ -18,7 +18,6 @@ int	main(void)
 	int fd1, fd2, fd3, fd4, fd5;
 	char *line;
 
-	// Abrindo múltiplos arquivos
 	fd1 = open("test2.txt", O_RDONLY);
 	if (fd1 < 0)
 	{
@@ -27,7 +26,6 @@ int	main(void)
 	}
 
 
-	// Leitura alternada entre os arquivos
 	while ((line = get_next_line(fd1)) != NULL)
 	{
 		if (!line)
@@ -36,7 +34,6 @@ int	main(void)
 		free(line);
 	}
 
-	// Fechando os arquivos
 	close(fd1);
 
 
